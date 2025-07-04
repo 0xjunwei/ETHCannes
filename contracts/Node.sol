b5d369e2932040c3e25e52ff44a00153c5ee90f3
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2025-07-05 03:37:52 +0800

    Add files via upload
    Creating the node for cross chain transfer, cctp v2 depositForBurn to pass usdc one to another, paymaster multichain

---
+++ contracts/Node.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ITokenMessengerV2 {
+    function depositForBurn(
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient,
+        address burnToken,
+        bytes32 destinationCaller,
+        uint256 maxFee,
+        uint32 minFinalityThreshold
+    ) external;
+}
+
+// Message transmitter receiver interface
+interface IMessageTransmitterV2 {
+    function receiveMessage(
+        bytes calldata message,
+        bytes calldata attestation
+    ) external returns (bool);
+
+    function sendMessage(
+        uint32 destinationDomain,
+        bytes32 recipient,
+        bytes32 destinationCaller,
+        uint32 minFinalityThreshold,
+        bytes calldata messageBody
+    ) external;
+}
+
+contract Node is Ownable, AccessControl {
+    bytes32 public constant AUTHORIZED = keccak256("AUTHORIZED");
+    // Address for usdc will never change immutable it to reduce gas
+    IERC20 public immutable usdc;
+    // CCTP Contracts
+    ITokenMessengerV2 public tokenMessenger;
+
+    // Stores their balance for allowance everytime they do a gasRelay
+    mapping(address => uint256) public usdcForGasRemaining;
+
+    event BurnInitiated(
+        address indexed caller,
+        address indexed user,
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient
+    );
+
+    constructor(address _usdc, address _tokenMessenger) Ownable(msg.sender) {
+        usdc = IERC20(_usdc);
+        tokenMessenger = ITokenMessengerV2(_tokenMessenger);
+        _grantRole(AUTHORIZED, msg.sender);
+    }
+
+    modifier onlyAuthorized() {
+        require(
+            owner() == msg.sender || hasRole(AUTHORIZED, msg.sender),
+            "Not authorized"
+        );
+        _;
+    }
+
+    // Handle paymaster USDC to USDC transfer
+    function depositForBurnCrossChain(
+        address user,
+        uint256 amount,
+        uint32 destinationDomain,
+        uint256 maxFee,
+        uint32 minFinalityThreshold
+    ) external onlyAuthorized {
+        require(amount > 0, "Amount zero");
+        usdc.transferFrom(user, address(this), amount);
+        // update the contract gas balance
+        uint256 remaining = usdc.allowance(user, address(this));
+        usdcForGasRemaining[user] = remaining;
+        bytes32 destinationUserAddress = bytes32(uint256(uint160(user)));
+
+        tokenMessenger.depositForBurn(
+            amount,
+            destinationDomain,
+            destinationUserAddress,
+            address(usdc),
+            bytes32(0), // allow any caller on destination
+            maxFee,
+            minFinalityThreshold
+        );
+
+        emit BurnInitiated(
+            msg.sender,
+            user,
+            amount,
+            destinationDomain,
+            destinationUserAddress
+        );
+    }
+
+    function withdrawTokens(
+        address tokenAddress,
+        uint256 amount
+    ) external onlyOwner {
+        require(tokenAddress != address(0), "Token address zero");
+        IERC20 token = IERC20(tokenAddress);
+        uint256 balance = token.balanceOf(address(this));
+        require(amount > 0 && amount <= balance, "Invalid token amount");
+        // Send to msg.sender as onlyOwner can call this function anyways
+        bool ok = token.transfer(msg.sender, amount);
+        require(ok, "Token transfer failed");
+    }
+}
