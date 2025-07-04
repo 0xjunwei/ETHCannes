7453f534c0e8ae925ba815455c3c96cd857bb634
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2025-07-05 03:39:12 +0800

    Add files via upload
    hardhat config with all my imports, planning to CCTP + Data Feeds to pull for prices

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+
+const ARBITRUM_SEPOLIA_RPC_URL = process.env.ARBITRUM_SEPOLIA_RPC_URL || "";
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 421614,
+    },
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 84532,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+};
